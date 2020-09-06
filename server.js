2ff58a331c261b6fcc0dc65f34c5ce7d67cf205c
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-09-06 16:08:51 +0300

    handle searches

--- server.js
+++ server.js
@@ -16,4 +16,23 @@ app.get("/", (req, res) => {
 app.get("/searches/new", (req, res) => {
     res.render("./pages/searches/new");
 });
+app.get("/searches", (req, res) => {
+    const url = `https://www.googleapis.com/books/v1/volumes?q=cat+title`;
+    console.log(url);
+    superagent.get(url).then(({ body }) => {
+        const books = body.items.map((book) => new Books(book));
+        res.render("pages/searches/show", { books });
+    });
+});
+function Books(data) {
+    console.log(data.volumeInfo.imageLinks);
+    if (data.volumeInfo.imageLinks === undefined) {
+        this.image = "https://i.imgur.com/J5LVHEL.jpg";
+    } else {
+        this.image = data.volumeInfo.imageLinks.thumbnail;
+    }
+    this.title = data.volumeInfo.title;
+    this.authors = data.volumeInfo.authors;
+    this.description = data.searchInfo.textSnippet;
+}
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
