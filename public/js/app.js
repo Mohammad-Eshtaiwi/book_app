29832f3f8cfdfa0cc1a37e100da5b6c22e1e4463
Mohammad-Eshtaiwi <mohd.eshtiwi@gmail.com>
2020-09-08 15:45:42 +0300

    lab-13

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,10 @@
 $(".menu").on("click", (event) => {
     $("nav ul").slideToggle();
 });
+
+// update button
+
+$("button.update").on("click", () => {
+    console.log("hiiiiiiii");
+    $(".update-book").toggleClass("d-none");
+});
--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const superagent = require("superagent");
 const pg = require("pg");
+const methodOverride = require("method-override");
+const { redirect } = require("express/lib/response");
 require("dotenv").config();
+
 // express setup
 const PORT = process.env.PORT || 3000;
 const client = new pg.Client(process.env.DATABASE_URL);
 app.use(express.static("./public"));
 app.use(express.urlencoded());
 app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
 // routes
 
 app.get("/", (req, res) => {
@@ -50,25 +54,71 @@ app.post("/addBook", (req, res) => {
     // console.log(req.body);
     const { title, image_url, author, isbn, description } = req.body;
     safeValues = [title, image_url, author, isbn, description];
-    const SQL = `INSERT INTO books (title, image_url, author, isbn, description) VALUES ($1,$2,$3,$4,$5)`;
-    client
-        .query(SQL, safeValues)
-        .then((results) => {
-            // console.log("sssssssssssssssss", results);
-            res.render("pages/books/details", { book: req.body });
-        })
-        .catch((err) => {
-            res.redirect("/");
-        });
+    let SQL = `INSERT INTO books (title, image_url, author, isbn, description) VALUES ($1,$2,$3,$4,$5)`;
+    client.query(SQL, safeValues).then((results) => {
+        SQL = `SELECT * FROM books WHERE isbn = $1`;
+        client
+            .query(SQL, [isbn])
+            .then((result) => {
+                res.redirect(`/books/${result.rows[0].id}`);
+            })
+            .catch((err) => {
+                res.redirect("/");
+            });
+    });
 });
+// (results) => {
+
+//     // console.log("sssssssssssssssss", results);
+//     SQL = `SELECT * FROM books WHERE isbn = $1`;
+//     client.query(SQL, [isbn]).then(result=>{
+//         res.redirect(`/books/${result.rows[0].id}`)
+//     }).catch((err) => {
+//         res.redirect("/");
+//     })
 // get book by id
 app.get("/books/:id", (req, res) => {
     // req.params.id
     const value = [req.params.id];
     const SQL = `SELECT * FROM books WHERE id = $1`;
     client.query(SQL, value).then(({ rows }) => {
-        rows[0].description = fixStr(rows[0].description);
+        // rows[0].description = fixStr(rows[0].description);
         res.render("pages/books/details", { book: rows[0] });
+    });
+});
+app.put("/updateBook/:id", (req, res) => {
+    // req.params.id
+    const { title, author, ibsn: isbn, description, image_url } = req.body;
+
+    // console.log(title);
+    // console.log(image_url);
+    // console.log(author);
+    // console.log(isbn);
+    // console.log(description);
+
+    const safeValues = [
+        title,
+        author,
+        isbn,
+        image_url,
+        description,
+        req.params.id,
+    ];
+    console.log(safeValues);
+    // const SQL = `INSERT INTO books (title, image_url, author, isbn, description) VALUES ($1,$2,$3,$4,$5)`;
+    const SQL = `UPDATE books SET title=$1,author=$2,isbn=$3,image_url=$4 , description=$5  where id = $6;`;
+    client.query(SQL, safeValues).then((results) => {
+        console.log(results);
+        res.redirect(`/books/${req.params.id}`);
+    });
+});
+// /deleteBook/
+app.delete("/deleteBook/:id", (req, res) => {
+    // req.params.id
+    const SQL = `DELETE FROM books WHERE id=$1`;
+    const value = [req.params.id];
+    client.query(SQL, value).then((results) => {
+        res.redirect("/");
     });
 });
 app.use("*", (req, res) => {
